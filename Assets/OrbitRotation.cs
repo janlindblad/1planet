a5dc62d9fda4947b428aae5e7bf62f9e80e096b0
Jan Lindblad <jan.lindblad@protonmail.com>
2016-11-23 22:33:59 +0100

    Added revolving 1planet logo, and jump to Status scene after a few secs

---
+++ Assets/OrbitRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OrbitRotation : MonoBehaviour {
+	public GameObject orbit;
+	public Light lightsrc;
+	float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		orbit.transform.RotateAround (Vector3.up, Vector3.up, speed);
+		speed *= 0.96f;
+		if(speed < 0.0001f) {
+			lightsrc.intensity *= 0.96f;
+			if(lightsrc.intensity < 0.0001f) {
+				SceneManager.LoadScene("Status");
+			}
+		}
+	}
+}
