2518866409412f31bd442071d1b40ed39e41d9e2
Jan Lindblad <jan.lindblad@protonmail.com>
2016-12-16 22:08:50 +0100

    Developed mission and missionball prefabs a little

--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -8,9 +8,12 @@ using System.IO;
 public class GameControl : MonoBehaviour {
 	[Serializable] public class PersistGameData {
 		public Boolean welcomed = false;
+		public MissionID[] completed_missions = {};
+		public MissionID[] ongoing_missions = {};
 	}
 
 	public static GameControl control;
+	public static MissionDB missiondb;
 	public PersistGameData pad;
 	String persistFileName;
 	BinaryFormatter bf = new BinaryFormatter ();
@@ -23,6 +26,7 @@ public class GameControl : MonoBehaviour {
 		} else if (control != this) {
 			Destroy (gameObject);
 		}
+		missiondb = new MissionDB ();
 	}
 	
 	void Update () {	
@@ -50,6 +54,73 @@ public class GameControl : MonoBehaviour {
 		} else {
 			Debug.Log("Created new state data");
 			return new PersistGameData();
+		}
+	}
+}
+
+public class MissionDB {
+	MissionID[] get_ids() {
+		return new MissionID[] { 
+			new MissionID("1.1"), 
+			new MissionID("1.2"),
+			new MissionID("1.3"),
+			new MissionID("1.4"),
+			new MissionID("1.5"),
+			new MissionID("1.6"),
+			new MissionID("1.7"),
+			new MissionID("1.8"),
+			new MissionID("1.9"),
+			new MissionID("1.10"),
+			new MissionID("1.11"),
+			new MissionID("1.12"),
+		};
+	}
+	//MissionID mid, string title, int size, string balltype, 
+	//int level, string short_desc, string long_desc,
+	//MissionID upgrade = null, MissionID downgrade = null
+	Mission get(MissionID mid) {
+		if (mid.id == "1:1") {
+			return new Mission (
+				new MissionID("1:1"), 
+				"Din köttfria dag",
+				2, "earth", 1,
+				"Under 24 timmar ska du undvika allt kött.",
+				"Ingen skinka till frukost, ingen kycklingfilé till lunch, ingen köttfärs till middag.\n\nFisk, ägg och blodpudding är ok under detta uppdrag. Dessa räknas inte som kött.",
+				new MissionID("1:2"),
+				null
+			);
+		} else if (mid.id == "1:2") {
+			return new Mission (
+				new MissionID("1:2"), 
+				"Din köttfria vecka",
+				3, "earth", 1,
+				"Under en vecka ska du undvika allt kött.",
+				"Ingen skinka till frukost, ingen kycklingfilé till lunch, ingen köttfärs till middag.\n\nFisk, ägg och blodpudding är ok under detta uppdrag. Dessa räknas inte som kött.",
+				null,
+				null
+			);
+		} else if (mid.id == "1:3") {
+			return new Mission (new MissionID("1:3"), "Din ultimata vecka 3", 3, "red", 1, "Red red red.", "Long red silver fox.", null, null	);
+		} else if (mid.id == "1:4") {
+			return new Mission (new MissionID("1:4"), "Din ultimata vecka 4", 5, "red", 1, "Red red red.", "Long red silver fox.", null, null	);
+		} else if (mid.id == "1:5") {
+			return new Mission (new MissionID("1:5"), "Din ultimata vecka 5", 8, "red", 1, "Red red red.", "Long red silver fox.", null, null	);
+		} else if (mid.id == "1:6") {
+			return new Mission (new MissionID("1:6"), "Din ultimata vecka 6", 4, "red", 1, "Red red red.", "Long red silver fox.", null, null	);
+		} else if (mid.id == "1:7") {
+			return new Mission (new MissionID("1:7"), "Din ultimata vecka 7", 2, "red", 1, "Red red red.", "Long red silver fox.", null, null	);
+		} else if (mid.id == "1:8") {
+			return new Mission (new MissionID("1:8"), "Din ultimata vecka 8", 5, "red", 1, "Red red red.", "Long red silver fox.", null, null	);
+		} else if (mid.id == "1:9") {
+			return new Mission (new MissionID("1:9"), "Din ultimata vecka 9", 1, "red", 1, "Red red red.", "Long red silver fox.", null, null	);
+		} else if (mid.id == "1:10") {
+			return new Mission (new MissionID("1:10"), "Din ultimata vecka 10", 0, "red", 1, "Red red red.", "Long red silver fox.", null, null	);
+		} else if (mid.id == "1:11") {
+			return new Mission (new MissionID("1:11"), "Din ultimata vecka 11", 1, "red", 1, "Red red red.", "Long red silver fox.", null, null	);
+		} else if (mid.id == "1:12") {
+			return new Mission (new MissionID("1:12"), "Din ultimata vecka 12", 9, "red", 1, "Red red red.", "Long red silver fox.", null, null	);
+		} else {
+			return null;
 		}
 	}
 }
--- Assets/Mission.cs
+++ Assets/Mission.cs
@@ -2,8 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mission : MonoBehaviour {
+public class MissionID {
+	public string id;
+	public MissionID(string id) {
+		this.id = id;
+	}
+	public override string ToString() {
+		return this.id;
+	}
+}
 
+public class Mission : MonoBehaviour {
+	public MissionID id;
+	public MissionID upgrade;
+	public MissionID downgrade;
 	public string title;
 	public int size;
 	public string balltype;
@@ -11,6 +23,21 @@ public class Mission : MonoBehaviour {
 	public string short_desc;
 	public string long_desc;
 	GameObject sphere;
+
+	public Mission(MissionID mid, string title, int size, string balltype, 
+		int level, string short_desc, string long_desc,
+		MissionID upgrade = null, MissionID downgrade = null) {
+		this.id = mid;
+		this.title = title;
+		this.size = size;
+		this.balltype = balltype;
+		this.level = level;
+		this.short_desc = short_desc;
+		this.long_desc = long_desc;
+		this.upgrade = upgrade;
+		this.downgrade = downgrade;
+	}
+
 
 	void Start () {
 		sphere = this.transform.FindChild("Sphere").gameObject;
