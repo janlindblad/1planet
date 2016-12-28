d892680679cd50cd2f684e312008d348d659a3dc
Jan Lindblad <jan.lindblad@protonmail.com>
2016-12-28 14:44:57 +0100

    Debriefing more like briefing

--- Assets/Debriefing.cs
+++ Assets/Debriefing.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Debriefing : MonoBehaviour {
 	public GameObject stars;
 	public GameObject missionGem;
+	public GameObject missionText;
 	float r;
+	float t;
 	// Use this for initialization
 	void Start () {
-		
+		t = 1;
 	}
 	
 	// Update is called once per frame
@@ -19,5 +21,7 @@ public class Debriefing : MonoBehaviour {
 			0.1f);
 		r += .01f;
 		missionGem.transform.rotation = Quaternion.Euler (new Vector3 (r, r, r));
+		t *= 0.9998f;
+		missionText.transform.localPosition = new Vector3 (0, 0.55f - t, 0);
 	}
 }
